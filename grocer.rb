578084bd08fc087a90223ea9b3bf34553895d99e
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-22 16:22:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,5 +56,5 @@ def checkout(cart, coupons)
     total += price_hash[:price] * price_hash[:count]
   end
   
-  total = (total * 0.9) if total > 100 total 
+  total #= (total * 0.9) if total > 100 total 
 end
